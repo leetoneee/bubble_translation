8a0e23d38c1ff328a15f9b34a9937840ce76a813
leetoneee <letoan7442@gmail.com>
2025-05-23 11:06:08 +0700

    fix merge commit

--- app/src/main/java/com/bteamcoding/bubbletranslation/feature_bubble_translation/presentation/PartialScreenModeAction.kt
+++
@@ -1,8 +0,0 @@
-package com.bteamcoding.bubbletranslation.feature_bubble_translation.presentation
-
-import com.google.mlkit.vision.text.Text
-
-sealed interface PartialScreenModeAction {
-    data class OnChange(val newText: Text) : PartialScreenModeAction
-    data object OnReset : PartialScreenModeAction
-}
